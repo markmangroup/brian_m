866aeebc9b5d7dca62b734bddc80e06348237d4e
Mike Markman <mike@markmanassociates.com>
2025-05-13 20:45:20 -0400

    chore: fully remove Gimkit from project and set Robot Lab as default route

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import Gimkit from './components/Gimkit';
 import PixelArtMaker from './components/PixelArtMaker';
 import RobotLab from './components/RobotLab';
 import LegoBuilder from './components/LegoBuilder';
@@ -13,9 +12,6 @@ function App() {
       <div className="min-h-screen bg-gray-900 text-white">
         <nav className="bg-gray-800 p-4">
           <div className="container mx-auto flex flex-wrap gap-4">
-            <Link to="/gimkit" className="px-4 py-2 rounded-full bg-purple-600 hover:bg-purple-500 transition-colors">
-              Gimkit
-            </Link>
             <Link to="/pixel-art" className="px-4 py-2 rounded-full bg-blue-600 hover:bg-blue-500 transition-colors">
               Pixel Art
             </Link>
@@ -36,14 +32,12 @@ function App() {
 
         <div className="container mx-auto p-4">
           <Routes>
-            <Route path="/gimkit" element={<Gimkit />} />
-            <Route path="/space-station" element={<Gimkit />} />
             <Route path="/pixel-art" element={<PixelArtMaker />} />
             <Route path="/robot-lab" element={<RobotLab />} />
             <Route path="/lego-builder" element={<LegoBuilder />} />
             <Route path="/lego-inventory" element={<LegoInventory />} />
             <Route path="/little-alchemy" element={<LittleAlchemy />} />
-            <Route path="/" element={<Gimkit />} />
+            <Route path="/" element={<RobotLab />} />
           </Routes>
         </div>
       </div>
