098189797ff6aaea72576c91b003ace9bbaa6af7
markmangroup <mike@markmanassociates.com>
2025-05-14 10:18:30 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
 import React from 'react';
-import SnackTrail from './components/SnackTrail';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+
+import SnackNav from './components/SnackNav';
 import SnackTrailHUD from './components/SnackTrailHUD';
+
+import PixelArtMaker from './components/PixelArtMaker';
+import RobotLab from './components/RobotLab';
+import LegoBuildMode from './components/LegoBuildMode';
+import LegoInventory from './components/LegoInventory';
+import LittleAlchemy from './components/LittleAlchemy';
+import SnackTrail from './components/SnackTrail';
 
 export default function App() {
   return (
-    <div className="min-h-screen bg-gray-900 text-white">
-      <SnackTrailHUD>
-        <SnackTrail />
-      </SnackTrailHUD>
-    </div>
+    <Router>
+      <div className="min-h-screen bg-gray-900 text-white pt-16 relative">
+        <SnackNav />
+        <Routes>
+          <Route path="/snack-trail" element={
+            <SnackTrailHUD>
+              <SnackTrail />
+            </SnackTrailHUD>
+          } />
+          <Route path="/pixel-art" element={<PixelArtMaker />} />
+          <Route path="/robot-lab" element={<RobotLab />} />
+          <Route path="/lego-build" element={<LegoBuildMode />} />
+          <Route path="/lego-inventory" element={<LegoInventory />} />
+          <Route path="/little-alchemy" element={<LittleAlchemy />} />
+          <Route path="*" element={<Navigate to="/snack-trail" />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
