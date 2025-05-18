4bee3a47eea3e5e9a59a9366c47b46e63716a8f9
markmangroup <mike@markmanassociates.com>
2025-05-18 11:03:17 -0400

    Fix Matrix routes

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ export default function App() {
           <Route path="/lego-inventory" element={<LegoInventory />} />
           <Route path="/little-alchemy" element={<LittleAlchemy />} />
           <Route path="/the-matrix" element={<TheMatrix />} />
-          <Route path="/the-matrix"            element={<MatrixTransition />} />
+          <Route path="/matrix-transition" element={<MatrixTransition />} />
   <Route path="/the-matrix/terminal"   element={<MatrixTerminal   />} />
   <Route path="/the-matrix/puzzle"     element={<MatrixPuzzle     />} />
 
